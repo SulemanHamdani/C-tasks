6698ed05bf53424b16d638a6ad767138035c24f4
SulemanHamdani <96333810+SulemanHamdani@users.noreply.github.com>
2022-08-24 17:35:25 +0500

    Update distance.cpp

--- distance.cpp
+++ distance.cpp
@@ -17,19 +17,27 @@ struct CartesianCoord
 		this->y = y;
 	}
 
-	float calculateDistance(float x1, float y1, float x2, float y2)
-	{
-		float distance = sqrt((pow(x2 - x1, 2)) + (pow(y2 - y1, 2)));
-		return distance;
-	}
+	
 };
+
+float calculateDistance(CartesianCoord c1, CartesianCoord c2)
+{
+	return sqrt((pow(c1.x - c2.x, 2)) + (pow(c1.y - c2.y, 2)));
+}
+
+int calculateChebyshevDistance(CartesianCoord c1, CartesianCoord c2)
+{
+	return max(abs(c1.x - c2.x), abs(c1.y - c2.y));
+}
+
 
 
 int main()
 {
 
 	CartesianCoord c1(0, 0), c2(4, 3);
-	cout << "The distance between ( " << c1.x << ", " << c1.y << " ) , ( " << c2.x << ", " << c2.y << " ) is: " << c1.calculateDistance(c1.x, c1.y, c2.x, c2.y);
+	cout << "The Euclidean distance between ( " << c1.x << ", " << c1.y << " ) , ( " << c2.x << ", " << c2.y << " ) is: " << calculateDistance(c1, c2) << endl;
+	cout << "The Chebyshev distance between ( " << c1.x << ", " << c1.y << " ) , ( " << c2.x << ", " << c2.y << " ) is: " << calculateChebyshevDistance(c1, c2);
 
 
 
