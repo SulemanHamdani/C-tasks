173feef5e93089bb8450fc0648bef44541a1a698
SulemanHamdani <96333810+SulemanHamdani@users.noreply.github.com>
2022-08-24 17:57:00 +0500

    Update distance.cpp

--- distance.cpp
+++ distance.cpp
@@ -22,12 +22,21 @@ struct CartesianCoord
 
 float calculateDistance(CartesianCoord c1, CartesianCoord c2)
 {
-	return sqrt((pow(c1.x - c2.x, 2)) + (pow(c1.y - c2.y, 2)));
+	float distanceSquared = sqrt((pow(c1.x - c2.x, 2)) + (pow(c1.y - c2.y, 2)));
+	return distanceSquared;
 }
 
 int calculateChebyshevDistance(CartesianCoord c1, CartesianCoord c2)
 {
 	return max(abs(c1.x - c2.x), abs(c1.y - c2.y));
+}
+
+float calculateMinkowskiDistance(CartesianCoord c1, CartesianCoord c2, float r ) 
+{
+	
+	float sum = 0;		
+	sum = pow(c1.x - c1.y, r) + pow(c2.x - c2.y, r); // here n can be 2 since we are dealing in 2d space. A for loop can be used for higher dimensions
+	return pow(sum, 1/r); // not working for values less than 1
 }
 
 
@@ -37,9 +46,9 @@ int main()
 
 	CartesianCoord c1(0, 0), c2(4, 3);
 	cout << "The Euclidean distance between ( " << c1.x << ", " << c1.y << " ) , ( " << c2.x << ", " << c2.y << " ) is: " << calculateDistance(c1, c2) << endl;
-	cout << "The Chebyshev distance between ( " << c1.x << ", " << c1.y << " ) , ( " << c2.x << ", " << c2.y << " ) is: " << calculateChebyshevDistance(c1, c2);
-
-
+	cout << "The Chebyshev distance between ( " << c1.x << ", " << c1.y << " ) , ( " << c2.x << ", " << c2.y << " ) is: " << calculateChebyshevDistance(c1, c2) << endl;
+	cout << "The Chebyshev distance between ( " << c1.x << ", " << c1.y << " ) , ( " << c2.x << ", " << c2.y << " ) is: " << calculateMinkowskiDistance(c1, c2, 2.0) << endl;
+	cout << pow(16, -2);
 
 	/*float x1, x2, y1, y2;
 	cout << "Enter the first pair of co-ordinates: ";
